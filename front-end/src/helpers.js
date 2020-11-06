3c2edffe0c4ccd07d981303b8e1b4fc25f0ff41e
s-ven-315 <lsinven@gmail.com>
2020-11-06 17:59:47 +0800

    Added AddNewRecipe function

--- front-end/src/helpers.js
+++ front-end/src/helpers.js
@@ -1,4 +1,9 @@
+import React from "react"
 import axios from "axios"
+import { useHistory, Redirect } from "react-router-dom"
+import { useState } from "react"
+
+
 
 export const Like = (recipeId, loggedIn) => {
     axios({
@@ -56,5 +61,80 @@ export const EditRecipeName = (loggedIn, recipeId, input) => {
             console.error(error.response)
         })
 }
+export const Follow = (userId, loggedIn) => {
+    axios({
+        method: 'POST',
+        url: `http://localhost:5000/api/users/${loggedIn.userId}/subscribe`,
+        headers: {
+            Authorization: "Bearer " + loggedIn.access_token
+        },
+        data: {
+            'userId': userId
+        }
+    })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.error(error.response)
+        })
+}
 
-export default { Like, Unlike, EditRecipeName }
+export const Unfollow = (userId, loggedIn) => {
+    axios({
+        method: 'POST',
+        url: `http://localhost:5000/api/users/${loggedIn.userId}/unsubscribe`,
+        headers: {
+            Authorization: "Bearer " + loggedIn.access_token
+        },
+        data: {
+            'userId': userId
+        }
+    })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.error(error.response)
+        })
+}
+export const EditProfileName = (loggedIn, userId, input) => {
+    axios({
+        method: 'POST',
+        url: `http://localhost:5000/api/users/${userId}/edit`,
+        headers: {
+            Authorization: "Bearer " + loggedIn.access_token
+        },
+        data: {
+            'name': input
+        }
+    })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.error(error.response)
+        })
+}
+export const AddNewRecipe = (loggedIn, userId, input, setCreateOpen) => {
+
+    axios({
+        method: 'POST',
+        url: 'http://localhost:5000/api/recipes/',
+        headers: {
+            Authorization: "Bearer " + loggedIn.access_token
+        },
+        data: {
+            'userId': userId,
+            'name': input
+        }
+    })
+        .then(response => {
+            console.log(response)
+            setCreateOpen(false)
+        })
+        .catch(error => {
+            console.error(error.response)
+        })
+}
+export default { Like, Unlike, EditRecipeName, Follow, Unfollow, EditProfileName, AddNewRecipe }
