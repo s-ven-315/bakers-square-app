a9c7860b425a268cc554ac89da4bc050bdd1e82b
s-ven-315 <lsinven@gmail.com>
2020-11-10 16:25:51 +0800

    Added DeleteRecipe function

--- front-end/src/helpers.js
+++ front-end/src/helpers.js
@@ -121,11 +121,13 @@ export const EditProfileName = (loggedIn, userId, input, setEditOpen, setNameCha
         .then(response => {
             console.log(response)
             setEditOpen(false)
+            setNameChanged(true)
         })
         .catch(error => {
             console.error(error.response)
         })
-    setNameChanged(true)
+    setNameChanged(false)
+
 }
 export const AddNewRecipe = (loggedIn, userId, input, setCreateOpen, history) => {
     const goToRecipePage = (recipeId) => {
@@ -165,4 +167,20 @@ export const AddNewRecipe = (loggedIn, userId, input, setCreateOpen, history) =>
             console.error(error.response)
         })
 }
-export default { Like, Unlike, EditRecipeName, Follow, Unfollow, EditProfileName, AddNewRecipe }
+
+export const DeleteRecipe = (loggedIn, recipeId, setDeleted) => {
+    axios({
+        method: 'POST',
+        url: `http://localhost:5000/api/recipes/${recipeId}/delete`,
+        headers: {
+            Authorization: "Bearer " + loggedIn.access_token
+        },
+        data: {
+        }
+    }).then(response => {
+        console.log(response)
+        setDeleted(true)
+    })
+    setDeleted(false)
+}
+export default { Like, Unlike, EditRecipeName, Follow, Unfollow, EditProfileName, AddNewRecipe, DeleteRecipe }
