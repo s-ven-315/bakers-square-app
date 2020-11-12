ec418523240c18c313d8034034aab4fc2b754ab5
Victor Tan <tanyiquan2@gmail.com>
2020-11-12 18:29:54 +0800

    Front-end: added redirect from homepage if logged in

--- front-end/src/pages/Homepage.js
+++ front-end/src/pages/Homepage.js
@@ -1,10 +1,17 @@
-import React from "react"
+import React, {useContext} from "react"
 import Button from '@material-ui/core/Button';
+import {Redirect} from "react-router-dom";
+import {DataContext} from "../contexts/Context";
 
 
 const Homepage = () => {
     console.log("Homepage() is rendered.")
+    const context = useContext(DataContext)
+    const {authUser} = context
 
+    if (authUser.access_token) {
+        return <Redirect to={`/users/${authUser.userId}`} />
+    }
     return (
         <>
             <div className="banner-container">
